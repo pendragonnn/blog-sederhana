c8c6620ba6e72eff99a93fe2e6e2dab81877f49a
Wisnu Andika <wisnuandika104@gmail.com>
2023-07-04 19:47:02 +0700

    menerapkan multiple useEffect

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -20,6 +20,10 @@ function Homepage() {
       .then((json) => setExternalPosts(json))
   }, []);
 
+  useEffect(() => {
+    console.log('ada post baru')
+  }, [posts]);
+
   return (
     <>
       <h1>Simple Blog</h1>
