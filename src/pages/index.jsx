752b50dd884862f663ec6dd7c68028bfe38f2479
Wisnu Andika <wisnuandika104@gmail.com>
2023-07-04 19:30:01 +0700

    menghubungkan API dengan useEffect

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import postsData from '../posts.json';
 import Article from '../components/Article';
 import Search from '../components/Search';
@@ -6,12 +6,19 @@ import Search from '../components/Search';
 function Homepage() {
   const [posts, setPosts] = useState(postsData);
   const [totalPosts, setTotalPosts] = useState(0);
+  const [externalPosts, setExternalPosts] = useState( [] );
 
   const onSearchChange = (value) => {
     const filteredPosts = postsData.filter(item => item.title.includes(value));
     setPosts(filteredPosts);
     setTotalPosts(filteredPosts.length);
   };
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then((json) => setExternalPosts(json))
+  }, []);
 
   return (
     <>
@@ -23,6 +30,11 @@ function Homepage() {
         //spread attributes
         <Article {...props} key={index}/>
       ))}
+      <h2>External Posts</h2>
+      {externalPosts.map((item, index) => (
+        <div key={index}>- {item.title}</div>
+      ))}
+      
     </>
   );
 }
