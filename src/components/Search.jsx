57a1e4639ccb63986e04d30d396bcd1fe1f6eb76
Wisnu Andika <wisnuandika104@gmail.com>
2023-07-04 18:28:44 +0700

    menambahkan tombol dengan onclick method

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,14 +4,20 @@ function Search(props) {
   const [search, setSearch] = useState("");
 
   const onSearchChange = (event) => {
-    setSearch(event.target.value);
-    props.onSearchChange(event.target.value);
+    props.onSearchChange(search);
+  };
+
+  const searchKeydown = (e) => {
+    if(e.key === 'Enter') {
+      onSearchChange();
+    }
   };
 
   return (
     <>
       <div>
-        Cari Artikel : <input onChange={onSearchChange}></input>
+        Cari Artikel : <input onChange={(e) => setSearch(e.target.value)} onKeyDown={searchKeydown}></input>
+        <button onClick={onSearchChange}>Cari</button>
       </div>
       <small>Ditemukan {props.totalPosts} data dengan pencarian kata {search}</small>  
     </>
