fe833519453a2b0f0428dc2e107506c2a1bfdea3
Wisnu Andika <wisnuandika104@gmail.com>
2023-07-04 12:47:24 +0700

    menerapkan fitur filter data

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,26 @@
+import { useState } from 'react';
 import posts from '../posts.json';
 import Article from '../components/Article';
 
 function Homepage() {
+  const [search, setSearch] = useState("");
+
+  const changeSearch = (event) => {
+    setSearch(event.target.value)
+  };
   return (
     <>
       <h1>Simple Blog</h1>
-      {posts.map((blog) => (
-          <Article title={blog.title} tags={blog.tags} date={blog.date}/>
-        ))}
+      <div>
+        Cari Artikel : <input onChange={changeSearch}></input>
+      </div>
+      <small>Ditemukan 0 data dengan pencarian kata {search}</small>
+      {posts.map(({title, tags, date}, index) => (
+        // <Article title={title} tags={tags} date={date}/>
+
+        //spread attributes
+        <Article {...{title, tags, date}} key={index}/>
+      ))}
     </>
   )
 }
