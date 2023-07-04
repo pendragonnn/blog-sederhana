4afb4a3d4cf03c2ac64cdb8bb03b6927fd81ae2c
Wisnu Andika <wisnuandika104@gmail.com>
2023-07-04 20:19:03 +0700

    routing menggunakan react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import { GlobalContext } from './context';
-import Home from './pages/index'
+import Home from './pages/index';
+import { router } from './routers';
 
 function App() {
   const user = {
     username: 'wisnu'
   };
-  
+
   return (
     <div className='App'>
       <GlobalContext.Provider value={user}>
-        <Home />
+        <RouterProvider router={router} />
       </GlobalContext.Provider>
     </div>
   )
