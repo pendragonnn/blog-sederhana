8deb3e0cd334722c057d57cee56ca7deb698312b
Wisnu Andika <wisnuandika104@gmail.com>
2023-07-05 13:14:15 +0700

    menerapkan dynamic style untuk navlink

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,9 +1,40 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
+import "../style/index.css";
 
 function RootLayout () {
   return (
     <>
-      <Link to="/">Home</Link> | <Link to='/about'>About</Link> | <Link to='/blog'>Blog</Link>
+      <NavLink className={({ isActive, isPending }) => 
+      isActive ? "active" : isPending ?  "pending" : ""
+      } 
+      style={{
+        marginRight: "5px"
+      }}
+      to="/">
+        Home
+      </NavLink>| 
+      
+      <NavLink className={({ isActive, isPending }) => 
+      isActive ? "active" : isPending ?  "pending" : ""
+      }
+      style={{
+        marginRight: "5px",
+        marginLeft: "5px"
+      }}
+      to='/about'>
+        About
+      </NavLink>| 
+      
+      <NavLink className={({ isActive, isPending }) => 
+      isActive ? "active" : isPending ?  "pending" : ""
+      } 
+      style={{
+        marginLeft: "5px"
+      }}
+      to='/blog'>
+        Blog
+      </NavLink>
+      
       <p/>
       <Outlet/>
     </>  
